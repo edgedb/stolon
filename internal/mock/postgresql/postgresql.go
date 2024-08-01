05dfcd4dd3b80855736264af607cb71d9ad2d666
Fantix King <fantix.king@gmail.com>
2024-08-01 13:35:16 -0400

    Regenerate test mock files (#7)
    Also added `make generate` command

--- internal/mock/postgresql/postgresql.go
+++ internal/mock/postgresql/postgresql.go
@@ -5,43 +5,46 @@
 package mocks
 
 import (
+	reflect "reflect"
+
 	gomock "github.com/golang/mock/gomock"
 	postgresql "github.com/sorintlab/stolon/internal/postgresql"
-	reflect "reflect"
 )
 
-// MockPGManager is a mock of PGManager interface
+// MockPGManager is a mock of PGManager interface.
 type MockPGManager struct {
 	ctrl     *gomock.Controller
 	recorder *MockPGManagerMockRecorder
 }
 
-// MockPGManagerMockRecorder is the mock recorder for MockPGManager
+// MockPGManagerMockRecorder is the mock recorder for MockPGManager.
 type MockPGManagerMockRecorder struct {
 	mock *MockPGManager
 }
 
-// NewMockPGManager creates a new mock instance
+// NewMockPGManager creates a new mock instance.
 func NewMockPGManager(ctrl *gomock.Controller) *MockPGManager {
 	mock := &MockPGManager{ctrl: ctrl}
 	mock.recorder = &MockPGManagerMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockPGManager) EXPECT() *MockPGManagerMockRecorder {
 	return m.recorder
 }
 
-// GetTimelinesHistory mocks base method
+// GetTimelinesHistory mocks base method.
 func (m *MockPGManager) GetTimelinesHistory(timeline uint64) ([]*postgresql.TimelineHistory, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetTimelinesHistory", timeline)
 	ret0, _ := ret[0].([]*postgresql.TimelineHistory)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
-// GetTimelinesHistory indicates an expected call of GetTimelinesHistory
+// GetTimelinesHistory indicates an expected call of GetTimelinesHistory.
 func (mr *MockPGManagerMockRecorder) GetTimelinesHistory(timeline interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetTimelinesHistory", reflect.TypeOf((*MockPGManager)(nil).GetTimelinesHistory), timeline)
 }
